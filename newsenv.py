4e675c2b1d5b0590d4e9c39471754c564c0409f6
DesikRengarajan <desik.29@gmail.com>
2020-09-10 18:28:48 -0500

    Added simple news

--- newsenv.py
+++ newsenv.py
@@ -50,6 +50,55 @@ class news:
 			return reward
 
 
+class simple_news:
+	def __init__(self,num_users=10,num_actions=5):
+		self.num_actions=num_actions
+		self.num_users=num_users
+		self.actions=[i for i in range(self.num_actions)]
+		self.users=[i for i in range(self.num_users)]
+		self.tod=[i for i in range(24)]
+
+	def get_context(self):
+		u=np.random.choice(self.users)
+		t=np.random.choice(self.tod)
+		context={'user':u,'tod':t}
+		return context 
+
+	def get_cost(self,context,action):
+		u=int(context['user'])
+		t=int(context['tod'])
+		
+		if ((u==0) and (t<5)):
+			if(action%2==0):
+				return -1.0
+		if ((u%3==0) and (t<15)):
+			if(action%2==0):
+				return -1.0
+		if ((u==1) and (t<20)):
+			if(action%3!=0):
+				return -1.0
+		if ((u%2==0) and (t<12)):
+			if(action==1):
+				return -1.0
+		if ((u%2==0) and (t<20)):
+			if(action==3):
+				return -1.0
+		if ((u==5) and (t<20)):
+			if(action==4):
+				return -1.0
+		if ((u==7) and (t<20)):
+			if(action==4):
+				return -1.0
+		
+		return 0.0					
+
+
+		
+
+
+
+
+
 
 
 
